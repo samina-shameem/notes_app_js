6b8dcc0c51deaeef00738dd209d9584d574846e7
samina-shameem <samina.shameem.clt@gmail.com>
2025-01-11 16:44:05 +0100

    worked note area.

---
+++ script.js
@@ -0,0 +1,21 @@
+const notesContainer=document.querySelector(".notes-container");
+const createBtn=document.querySelector(".btn");
+let notes=document.querySelector(".input-box");
+
+createBtn.addEventListener("click", ()=>{
+    let inputBox=document.createElement("p");
+    let img=document.createElement("img");
+    inputBox.className="input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src="images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+
+});
+
+notesContainer.addEventListener("click", function(e){
+       if(e.target.tagName=== "IMG"){
+          e.target.parentElement.remove();
+       }
+});
+
+
