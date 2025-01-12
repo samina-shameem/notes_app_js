29162460a1b380477cd0d1a96d7da52a9135372c
samina-shameem <samina.shameem.clt@gmail.com>
2025-01-12 19:42:00 +0100

    some changes,console log, code was successfull.

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-const notesContainer=document.querySelector(".notes-container");
-const createBtn=document.querySelector(".btn");
-let notes=document.querySelector(".input-box");
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
 
-createBtn.addEventListener("click", ()=>{
-    let inputBox=document.createElement("p");
-    let img=document.createElement("img");
-    inputBox.className="input-box";
+function showNotes(){
+    notesContainer.innerHTML=localStorage.getItem("notes");
+    //console.log("test2");
+}
+showNotes();
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+    //console.log("test1");
+}
+createBtn.addEventListener("click", () => {
+    let inputBox = document.createElement("P");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
     inputBox.setAttribute("contenteditable", "true");
-    img.src="images/delete.png";
+    img.src = "images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
 
-});
+})
 
-notesContainer.addEventListener("click", function(e){
-       if(e.target.tagName=== "IMG"){
-          e.target.parentElement.remove();
-       }
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }        
+        })
+       }        
+})
+
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
 });
 
 
